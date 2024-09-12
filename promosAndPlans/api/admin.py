c096abb8481f4d9d6d9f26201a4326a3159f58a4
Sejal Jain <164870150+SejalJain1710@users.noreply.github.com>
2024-09-12 22:21:46 +0530

    Delete promosAndPlans directory

--- promosAndPlans/api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Plan, Promo, UserSelectedPlans, User
-# Register your models here.
-
-admin.site.register(Plan)
-admin.site.register(Promo)
-admin.site.register(UserSelectedPlans)
-admin.site.register(User)
-
